4b0e385f7286e857831c8b8da80da422e5cbb17c
Alberto Sperindio <alberto@MacBook-Pro-di-Alberto.local>
2022-02-03 18:35:07 +0200

    CSS for expansion and menu items, lesson 73

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import "./menu-item.component.styles.scss";
 
-const MenuItem = ({ title }) => {
+const MenuItem = ({ title, imageUrl, size }) => {
   return (
-    <div className="menu-item">
+    <div className={`${size} menu-item`}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
       <div className="content">
-        <h1 className="title">{title}</h1>
+        <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">Shop now</span>
       </div>
     </div>
