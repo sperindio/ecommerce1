f3301c678c261f001531067546f16bcfe3649ed9
Alberto Sperindio <alberto@MacBook-Pro-di-Alberto.local>
2022-02-08 00:18:25 +0200

    Updated routing and downgraded react-router-dom to v5. There no Switch and HOC in >v6

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <HomePage />
-      </div>
-    );
-  }
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
