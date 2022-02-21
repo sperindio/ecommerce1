636d38e1cf2e6e456c8a2a0f8edf17eac96da84d
Alberto Sperindio <alberto@MacBook-Pro-di-Alberto.local>
2022-02-21 17:50:04 +0200

    Finished implementing Firebase sign in and sign out + styles

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -45,13 +45,14 @@ class SignIn extends React.Component {
             label="Password"
             required
           />
-
-          <CustomButton type="submit" value="Submit form">
-            Sign in
-          </CustomButton>
-          <CustomButton onClick={signInWithGoogle}>
-            Sign in with Google
-          </CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit" value="Submit form">
+              Sign in
+            </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with Google
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
