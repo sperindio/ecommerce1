a5c4611725ab19108c03dfa0846525d320cdb6ea
Alberto Sperindio <alberto@MacBook-Pro-di-Alberto.local>
2022-03-21 17:58:04 +0200

    Added new cart-types and actions to remove items in the cart

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,10 +1,13 @@
 //React
 import React from "react";
-
+//Redux
+import { connect } from "react-redux";
+import { clearItemFromCart } from "../../redux/cart/cart.actions";
 //Styles
 import "./checkout-item.styles.scss";
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => {
+const CheckoutItem = ({ cartItem, clearItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem; //Destructuring needs to be done afterwards since if it's done on top (passed in the function), the entire component would not have access to the entire cartItem, but just the destructured items.
   return (
     <div className="checkout-item">
       <div className="image-container">
@@ -13,9 +16,14 @@ const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => {
       <span className="name">{name}</span>
       <span className="quantity">{quantity}</span>
       <span className="price">{price}</span>
-      <div className="remove-button">&#10005;</div>
+      <div className="remove-button" onClick={() => clearItem(cartItem)}>
+        &#10005;
+      </div>
     </div>
   );
 };
 
-export default CheckoutItem;
+const mapDispatchToProps = (dispatch) => ({
+  clearItem: (item) => dispatch(clearItemFromCart(item)),
+});
+export default connect(null, mapDispatchToProps)(CheckoutItem);
