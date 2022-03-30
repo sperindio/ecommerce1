79b642fb1ae2ad10f116a9feaab611a221712234
Alberto Sperindio <alberto@MacBook-Pro-di-Alberto.local>
2022-03-30 17:40:40 +0300

    Added the cart persistence with redux-persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,19 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"], //Whitelist is used to indicate what reducer we want to make persisting. The user is managed by Firebase in this case, so we pass only cart.
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 
 const middlewares = [logger]; //In middlewares, we store the logger array, which intercept the set of actions that are being actionated.
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares)); //Now, we actually create the store with the root reducer (thus the collection of states) and the logger (actions)
+export const store = createStore(rootReducer, applyMiddleware(...middlewares)); //Now, we actually create the store with the root reducer (thus the collection of states) and the logger (actions)
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
