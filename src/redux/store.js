9cc09242fac17380e9f1778e954cb5b69216d48f
Alberto Sperindio <alberto@MacBook-Pro-di-Alberto.local>
2022-04-09 18:05:31 +0300

    removed logger from production version

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,11 @@ import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 
-const middlewares = [logger]; //In middlewares, we store the logger array, which intercept the set of actions that are being actionated.
+const middlewares = []; //In middlewares, we store the logger array, which intercept the set of actions that are being actionated.
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares)); //Now, we actually create the store with the root reducer (thus the collection of states) and the logger (actions)
 
