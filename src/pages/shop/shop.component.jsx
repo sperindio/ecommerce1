079213623cf6115a672615fd94658f2ce825f6b0
Alberto Sperindio <alberto@MacBook-Pro-di-Alberto.local>
2022-04-13 17:15:59 +0300

    Moved the async fetching data for Shop Items into Redux with Redux-Thunk

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,47 +1,46 @@
 import React from "react";
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 import { Route } from "react-router-dom";
+import { createStructuredSelector } from "reselect";
 import CollectionPage from "../collection/collection.component";
-import {
-  firestore,
-  convertCollectionsSnapshotToMap,
-} from "../../firebase/firebase.utils";
+
+//Selectors
+import { selectIsCollectionFetching } from "../../redux/shop/shop.selectors";
+
 import { connect } from "react-redux";
-import updateCollections from "../../redux/shop/shop.actions";
+import { fetchCollectionsStartAsync } from "../../redux/shop/shop.actions";
 import WithSpinner from "../../components/with-spinner/with-spinner.component";
 
 const CollectionOverviewWithSpinner = WithSpinner(CollectionsOverview);
 const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      loading: true,
-    };
-  }
-
-  //Fetch shop data from Firestor
-  unsubscribeFromSnapshot = null;
+  /*   //Fetch shop data from Firestor
+  unsubscribeFromSnapshot = null; */
 
   componentDidMount() {
-    const { updateCollections } = this.props;
-    const collectionRef = firestore.collection("collections");
+    const { fetchCollectionsStartAsync } = this.props;
+    fetchCollectionsStartAsync();
 
+    /* 
+   //EVERYTHING IS MANAGED BY REDUX AND REDUX-THUNK SINCE IT'S A BETTER PRACTICE. SHOP ITEMS COULDN'T LOAD UNLESS SHOP PAGE WAS VISITED
+   const { updateCollections } = this.props;
+    const collectionRef = firestore.collection("collections");
+//----------------------------------------------------
     /*     //The "fetch" method is similiar to the get() method, but the object returned is far more articulated than the others.
     fetch(
       "https://firestore.googleapis.com/v1/projects/crwn-db-4e424/databases/(default)/documents/collections"
     )
       .then((response) => response.json())
       .then((collections) => console.log(collections)); */
-
-    //This way is a one-time fetch method, quite common and doesn't require a listener component
+    //----------------------------------------------------
+    /*     //This way is a one-time fetch method, quite common and doesn't require a listener component
     collectionRef.get().then((snapshot) => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
       this.setState({ loading: false });
-    });
-
+    }); */
+    //----------------------------------------------------
     /*     
       //This method to fetch data is more advanced. It establishes a live listener on the DB or whatever event-generating component.
       this.unsubscribeFromSnapshot = collectionRef.onSnapshot(
@@ -54,22 +53,27 @@ class ShopPage extends React.Component {
   }
 
   render() {
-    const { match } = this.props;
-    const { loading } = this.state;
+    const { match, isCollectionFetching } = this.props;
     return (
       <div className="shop-page">
         <Route
           exact
           path={`${match.path}`}
           render={(props) => (
-            <CollectionOverviewWithSpinner isLoading={loading} {...props} />
+            <CollectionOverviewWithSpinner
+              isLoading={isCollectionFetching}
+              {...props}
+            />
           )}
         />
         <Route
           exact
           path={`${match.path}/:collectionId`}
           render={(props) => (
-            <CollectionPageWithSpinner isLoading={loading} {...props} />
+            <CollectionPageWithSpinner
+              isLoading={isCollectionFetching}
+              {...props}
+            />
           )}
         />
       </div>
@@ -77,8 +81,11 @@ class ShopPage extends React.Component {
   }
 }
 
-const mapDispatchToProps = (dispatch) => ({
-  updateCollections: (collectionsMap) =>
-    dispatch(updateCollections(collectionsMap)),
+const mapStateToProps = createStructuredSelector({
+  isCollectionFetching: selectIsCollectionFetching,
 });
-export default connect(null, mapDispatchToProps)(ShopPage);
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync()),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(ShopPage);
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,9 @@ import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 import rootReducer from "./root-reducer";
+import thunk from "redux-thunk";
 
-const middlewares = []; //In middlewares, we store the logger array, which intercept the set of actions that are being actionated.
+const middlewares = [thunk]; //In middlewares, we store the logger array, which intercept the set of actions that are being actionated.
 
 if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
